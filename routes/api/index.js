6435104195b744fa4539774d85f436ea7a7406d2
Bri <bhilliker@gmail.com>
2021-08-24 21:18:23 -0500

    added post-routes to manipulate posts

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,15 @@
 const router = require('express').Router();
-const userRoutes = require('./user-routes.js');
-router.use('/users', userRoutes);
+const userRoutes = require('./user-routes');
+const postRoutes = require('./post-routes');
+const { Post, User } = require('../../models/');
 
+router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
+
+
+router.use((req, res) => {
+    res.status(404).end();
+});
 
 
 module.exports = router;
